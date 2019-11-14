0931e5a3ec90e88953ccfd8f6c14498b13ecae71
jonsh96 <jonsh96@outlook.com>
2019-11-14 13:25:34 +0000

    .

--- Fama_MacBeth.m
+++ Fama_MacBeth.m
@@ -1,5 +1,6 @@
 %% TODO:
 %  - Fama MacBeth regression
 %  - https://uk.mathworks.com/matlabcentral/answers/280444-query-regarding-fama-macbeth-regression
+%  - https://faculty.chicagobooth.edu/john.cochrane/teaching/35150_advanced_investments/week5a_notes.pdf
 liquidity_data.Properties.VariableNames
 
